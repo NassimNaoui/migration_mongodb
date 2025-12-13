8ed4a156c9d16e5b30a1b439e9cfd2acde397ce2
Nassanalytics <nassim.naoui@gmail.com>
2025-12-13 11:09:25 +0100

    test extrac transform

--- app/sample/main.py
+++ app/sample/main.py
@@ -1,4 +1,5 @@
 from extract.csv_reader import csv_reader
+from transform.data_cleaning import data_transform
 from db_manager import mongo_manager
 
 def main():
@@ -6,6 +7,21 @@ def main():
         print("✅ connexion to MongoDB succeed : The server is online")
     else:
         print("❌ connexion to mongoDB failed : The server is offline")
+    
+
+    reader = csv_reader(file_path='data/healthcare_dataset.csv')
+    cleaner = data_transform()
+
+
+    data = reader.read_data(1,100, 100)
+    data_transformed = cleaner.transform_data(data)
+    data_transformed = cleaner.count_admission_id(data_transformed)
+    document_to_load = cleaner.convert_df_into_doc(data_transformed)
+
+    users = mongo_manager.get_collection()
+
+    users.insert_many(document_to_load)
+
 
 if __name__ == "__main__":
     main()
--- app/sample/transform/data_cleaning.py
+++ app/sample/transform/data_cleaning.py
@@ -8,31 +8,35 @@ class data_transform:
         df_transformed = df.copy()
         
         try:
-            # --- 1. drop duplicates rows ---
+            # --- drop duplicates rows ---
             df_transformed = df_transformed.drop_duplicates()
 
-            # --- 2. return name in upper case ---
+            # --- change column name ---
+            col_name = df_transformed.columns.to_list()
+            new_col_name = list(map(lambda x: x.replace(" ", "_"), col_name))
+            new_col_name = list(map(lambda x: x.lower(), new_col_name))
+            df_transformed.columns = new_col_name
+
+            # --- return name in upper case ---
             df_transformed["name"] =  df_transformed["name"].str.upper()
 
-            # --- 3. Round with 2 decimales the billing amout ---
+            # --- Delete title in Name
+            df_transformed['name'] = df_transformed['name'].str.upper().str.replace(r'\b(MRS|MR|MS|DR)\.?\s*', '', regex=True).str.strip()
+
+            # --- return the first name col and last name col ---
+            df_transformed[['first_name', 'last_name']] = df_transformed['name'].str.strip().str.split(r'\s+', n=1, expand=True)
+
+            # --- Round with 2 decimales the billing amout ---
+            df_transformed["billing_amount"] = df_transformed["billing_amount"].astype(float)
             df_transformed["billing_amount"] = round(df_transformed["billing_amount"],2)
 
-            # --- 4. return the first name col and last name col ---
-            df_transformed[['first_name', 'last_name']] = df_transformed['name'].str.split(' ', expand=True)
-
-            # --- 5. convert str to date format ---
+            # --- convert str to date format ---
             col_to_format = ["date_of_admission", "discharge_date"]
             for col in col_to_format:
                 df_transformed[col] = pd.to_datetime(df_transformed[col])
 
-            # --- 6. change column name ---
-            col_name = df_transformed.columns.to_list()
-            new_col_name = list(map(lambda x: x.replace(" ", "_"), col_name))
-            new_col_name = list(map(lambda x: x.lower(), col_name))
-            df_transformed.columns = new_col_name
-
-            # --- 7. adding unique key for each patient 
-            df_transformed['key'] = df_transformed["name"] + "_" +  df_transformed["age"] + "_" + df_transformed["gender"] + "_" + df_transformed["blood_type"]
+            # --- adding unique key for each patient 
+            df_transformed['key'] = df_transformed["name"] + "_" +  df_transformed["age"].astype(str) + "_" + df_transformed["gender"] + "_" + df_transformed["blood_type"]
 
 
             return df_transformed
@@ -46,8 +50,8 @@ class data_transform:
         # --- Count the number of admissions for each patient ---
 
         try:
-            df_filtered.sort_values(['name','Age','gender','blood_type'], ascending=True)
-            df_filtered['admission_id'] = df_filtered.groupby(['name','Age','gender','blood_type']).cumcount() + 1
+            df_filtered.sort_values(['name','age','gender','blood_type'], ascending=True)
+            df_filtered['admission_id'] = df_filtered.groupby(['name','age','gender','blood_type']).cumcount() + 1
             return df_filtered
         except Exception as e:
             print(f'error during the transformation process : {e}')
