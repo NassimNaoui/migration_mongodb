7f7a9b04baa06f9deb293963a39541054517c629
Nassanalytics <nassim.naoui@gmail.com>
2025-12-16 16:30:25 +0100

    test extrac done

--- app/tests/test_extract.py
+++ app/tests/test_extract.py
@@ -1,7 +1,9 @@
 from app.sample.extract.csv_reader import csv_reader
+import pandas as pd
 
 def test_csv_reader():
     reader = csv_reader('app/sample/data/healthcare_dataset.csv')
     data = reader.read_data(100,100)
 
     assert len(data) > 1
+    assert data.shape == (100, 15)
