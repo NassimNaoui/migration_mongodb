ae6f459711d976b8c135250b9ee3df451dbdbc8a
Nassanalytics <nassim.naoui@gmail.com>
2025-12-18 17:13:08 +0100

    test loader with mock done

---
+++ app/tests/test_load.py
@@ -0,0 +1,8 @@
+from unittest.mock import patch, MagicMock
+import app.sample.main as main_module
+
+@patch('app.sample.main.data_loader')
+def test_main_data_loader(mock_loader):
+    mock_loader.return_value.load_many_docs = MagicMock()
+    main_module.main()
+    mock_loader.return_value.load_many_docs.assert_called()
